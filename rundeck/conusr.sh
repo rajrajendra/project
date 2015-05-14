f3105ea3c14e8f7920749a14d424b7a2cbb88eb7
rajrajendra <peddadabrp@gmail.com>
2015-05-14 14:28:29 -0500

    adding new data

---
+++ rundeck/conusr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+echo "The script starts now"
+
+echo "Hi, $USER!"
+echo
+
+echo "I will now fech you a list of connected users:"
+echo
+w
+echo
+
+echo "I'm setting two veriables now."
+COLOUR="black"
+VALUE="9"
+echo "This is string: $COLOUR"
+echo "Add this is a number: $VALUE"
+echo
+
+echo "I'm giving you back your prompt now."
+echo
